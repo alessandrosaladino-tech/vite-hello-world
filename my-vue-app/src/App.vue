d5168f2e22d05331081a985482ed75051b060551
alessandrosaladino-tech <alessandro.saladino22@gmail.com>
2023-09-21 15:10:09 +0200

    Add
    vue object

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,13 +1,17 @@
 <script>
-export default{
-    
+export default {
+  data() {
+    return {
+
+      title: "Hello Vite",
   }
+  }
+}
 </script>
 
 <template>
- <h1>Hello Vue + Vite</h1>
+  <h1>Hello Vue + Vite</h1>
+  <h2>{{ title }}</h2>
 </template>
 
-<style>
-
-</style>
+<style></style>
